25c25347c0d32b82d22a43adaacc1fae35642702
iulialupu <iulia.lupu.19@gmail.com>
2019-04-13 16:42:13 +0300

    delete-recipe modal component

---
+++ client/src/components/DeleteRecipeModal.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import { connect } from "react-redux";
+
+import { deleteRecipe } from "../actions/recipeActions";
+
+function DeleteRecipeModal(props) {
+  const { deleteRecipe, id, title } = props;
+
+  const onDeleteClick = () => {
+    deleteRecipe(id);
+  };
+
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Body
+        closeButton
+        style={{ borderBottom: "none", padding: "2rem 2.5rem" }}
+      >
+        <h5>{`Are you sure you want to delete ${title} recipe?`}</h5>
+      </Modal.Body>
+      <Modal.Footer style={{ borderTop: "none", padding: "1rem 2rem" }}>
+        <Button variant="danger" onClick={onDeleteClick}>
+          Remove
+        </Button>
+        <Button variant="secondary" onClick={props.onHide}>
+          Cancel
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default connect(
+  null,
+  { deleteRecipe }
+)(DeleteRecipeModal);
--- client/src/components/Home/Recipe.js
+++ client/src/components/Home/Recipe.js
@@ -7,42 +7,9 @@ import Ingredients from "../Ingredients";
 import RecipeTitle from "../RecipeTitle";
 import StarsRating from "../StarsRating";
 import Button from "react-bootstrap/Button";
+import DeleteRecipeModal from "../DeleteRecipeModal";
 
 function Recipe({ recipe }) {
-  // const recipe = {
-  //   id: 1,
-  //   title: "Cajun Chicken Pasta",
-  //   author: {
-  //     id: 1,
-  //     userName: "John Doe"
-  //   },
-  //   createDate: "Jun, 2 2015",
-  //   updateDate: "Dec, 6 2017",
-  //   rating: 3.4,
-  //   ingredients: [
-  //     "4 ounces linguine pasta",
-  //     "2 skinless, boneless chicken breast halves",
-  //     "2 teaspoons Cajun seasoning",
-  //     "2 tablespoons butter",
-  //     "1 red bell pepper, sliced",
-  //     "1 green bell pepper, sliced",
-  //     "4 fresh mushrooms, sliced",
-  //     "1 green onion, chopped",
-  //     "1 cup heavy cream",
-  //     "1/4 teaspoon dried basil",
-  //     "1/4 teaspoon lemon pepper",
-  //     "1/4 teaspoon salt",
-  //     "1/8 teaspoon garlic powder",
-  //     "1/8 teaspoon ground black pepper",
-  //     "1/4 cup grated Parmesan cheese"
-  //   ],
-  //   instructions: [
-  //     "Bring a large pot of lightly salted water to a boil. Add pasta and cook for 8 to 10 minutes or until al dente; drain.",
-  //     "Place the chicken and the Cajun seasoning in a plastic bag. Shake to coat. In a large skillet over medium heat, saute the chicken in butter or margarine until almost tender (5 to 7 minutes).",
-  //     "Add the red bell pepper, green bell pepper, mushrooms and green onion. Saute and stir for 2 to 3 minutes. Reduce heat.",
-  //     "Add the cream, basil, lemon pepper, salt, garlic powder and ground black pepper. Heat through. Add the cooked linguine, toss and heat through. Sprinkle with grated Parmesan cheese and serve."
-  //   ]
-  // };
   const {
     _id,
     title,
@@ -53,27 +20,25 @@ function Recipe({ recipe }) {
     updateDate
   } = recipe;
 
+  const [modalIsOpen, setModalState] = React.useState(false);
+
+  const modalClose = () => setModalState(false);
+
   return (
     <div className="recipe-card paper-bg">
       <div className="recipe-content">
         <Link to={`/recipe/${_id}`}>
           <RecipeTitle title={title} />
         </Link>
-
         <StarsRating rating={rating} />
-
         {/* <AboutRecipeInfo
           createDate={createDate}
           updateDate={updateDate}
           author={author}
         /> */}
-
         <HrDecoLine />
-
         <Ingredients ingredients={ingredients} />
-
         <HrDecoLine />
-
         <div className="admin-btns d-flex justify-content-center">
           <Link
             to={`recipe/edit/${_id}`}
@@ -81,11 +46,22 @@ function Recipe({ recipe }) {
           >
             Edit
           </Link>
-          <Button variant="dark" className="px-3">
+          <Button
+            variant="dark"
+            className="px-3"
+            onClick={() => setModalState(true)}
+          >
             Delete
           </Button>
         </div>
       </div>
+
+      <DeleteRecipeModal
+        id={_id}
+        title={title}
+        show={modalIsOpen}
+        onHide={modalClose}
+      />
     </div>
   );
 }
